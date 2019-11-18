ab29965bc2223b31203b8a0c2eef838acb5cabfa
h0mdi <hamdijomaa.x@gmail.com>
2019-11-18 17:59:46 +0100

    navbar fixed width

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -68,179 +68,181 @@ const Navbar = props => {
   return (
     <Affix offsetTop={0}>
       <div className={"navbar " + onScrollClassName}>
-        <div className="brand">
-          <NavLink to={"/"}>
-            <Logo />
-          </NavLink>
-        </div>
-        {textSpecificPageDisblyed ? (
-          <div className="text-specific-pages">
-            <h3 className="gold-text">TUNISIA…</h3>
-            <h4 className={"bold-black"}>{textSpecificPageText}</h4>
+        <div className="fixed-with-container">
+          <div className="brand">
+            <NavLink to={"/"}>
+              <Logo />
+            </NavLink>
           </div>
-        ) : null}
-        <div className="navigation">
-          <NavLink
-            exact
-            className={"nav-link"}
-            activeClassName={"active"}
-            to={"/"}
-          >
-            {" "}
-            HI!
-          </NavLink>
-          <NavLink
-            className={"nav-link"}
-            activeClassName={"active"}
-            to={"/talents"}
-          >
-            {" "}
-            UX TALENTS
-          </NavLink>
-          <NavLink
-              className={"nav-link"}
-              activeClassName={"active"}
-              to={"/hosts"}
-          >
-            {" "}
-            UX HOSTS
-          </NavLink>
-          <NavLink
-            className={"nav-link"}
-            activeClassName={"active"}
-            to={"/events"}
-          >
-            {" "}
-            UX EVENTS
-          </NavLink>
-          <NavLink
-            className={"nav-link"}
-            activeClassName={"active"}
-            to={"/get-in-touch"}
-          >
-            {" "}
-            GET IN TOUCH
-          </NavLink>
-        </div>
-        <Motion style={{ x: spring(drawerOpened ? 255 : 0) }}>
-          {({ x }) => (
-            <div className="menu-icon" onClick={handleDrawerStatus}>
-              <span
-                style={{
-                  background: `rgb(${x}, ${x}, ${x})`
-                }}
-              />
-              <span
-                style={{
-                  background: `rgb(${x}, ${x}, ${x})`
-                }}
-              />
-              <span
-                style={{
-                  background: `rgb(${x}, ${x}, ${x})`
-                }}
-              />
-            </div>
-          )}
-        </Motion>
-
-        <Motion style={{ x: spring(drawerOpened ? 1 : 0) }}>
-          {({ x }) => (
-            <div
-              className="drawer"
-              style={{
-                opacity: x,
-                pointerEvents: drawerOpened ? "all" : "none"
-              }}
-            >
-              <div className="brand">
-                <NavLink to={"/"}>
-                  <LogoWhite />
-                </NavLink>
+          {textSpecificPageDisblyed ? (
+              <div className="text-specific-pages">
+                <h3 className="gold-text">TUNISIA…</h3>
+                <h4 className={"bold-black"}>{textSpecificPageText}</h4>
               </div>
-              <Motion
-                style={{
-                  x: spring(drawerOpened ? 0 : 200),
-                  y: spring(drawerOpened ? 0 : 400),
-                  w: spring(drawerOpened ? 0 : 600),
-                  z: spring(drawerOpened ? 0 : 800),
-                  o: spring(drawerOpened ? 1 : 0)
-                }}
-              >
-                {({ x, y, w, z, o }) => (
-                  <div className="menu-items">
-                    <NavLink
-                      exact
-                      className={"nav-link"}
-                      activeClassName={"active"}
-                      to={"/"}
+          ) : null}
+          <div className="navigation">
+            <NavLink
+                exact
+                className={"nav-link"}
+                activeClassName={"active"}
+                to={"/"}
+            >
+              {" "}
+              HI!
+            </NavLink>
+            <NavLink
+                className={"nav-link"}
+                activeClassName={"active"}
+                to={"/talents"}
+            >
+              {" "}
+              UX TALENTS
+            </NavLink>
+            <NavLink
+                className={"nav-link"}
+                activeClassName={"active"}
+                to={"/hosts"}
+            >
+              {" "}
+              UX HOSTS
+            </NavLink>
+            <NavLink
+                className={"nav-link"}
+                activeClassName={"active"}
+                to={"/events"}
+            >
+              {" "}
+              UX EVENTS
+            </NavLink>
+            <NavLink
+                className={"nav-link"}
+                activeClassName={"active"}
+                to={"/get-in-touch"}
+            >
+              {" "}
+              GET IN TOUCH
+            </NavLink>
+          </div>
+          <Motion style={{ x: spring(drawerOpened ? 255 : 0) }}>
+            {({ x }) => (
+                <div className="menu-icon" onClick={handleDrawerStatus}>
+              <span
+                  style={{
+                    background: `rgb(${x}, ${x}, ${x})`
+                  }}
+              />
+                  <span
                       style={{
-                        WebkitTransform: `translate3d(${-x}px, 0, 0)`,
-                        transform: `translate3d(${-x}px, 0, 0)`,
-                        opacity: o
+                        background: `rgb(${x}, ${x}, ${x})`
                       }}
-                    >
-                      {" "}
-                      Hi!
-                    </NavLink>
-                    <NavLink
-                      className={"nav-link"}
-                      activeClassName={"active"}
-                      to={"/talents"}
+                  />
+                  <span
                       style={{
-                        WebkitTransform: `translate3d(${-y}px, 0, 0)`,
-                        transform: `translate3d(${-y}px, 0, 0)`,
-                        opacity: o
+                        background: `rgb(${x}, ${x}, ${x})`
                       }}
-                    >
-                      {" "}
-                      UX Talents
-                    </NavLink>
-                    <NavLink
-                        className={"nav-link"}
-                        activeClassName={"active"}
-                        to={"/hosts"}
-                        style={{
-                          WebkitTransform: `translate3d(${-y}px, 0, 0)`,
-                          transform: `translate3d(${-y}px, 0, 0)`,
-                          opacity: o
-                        }}
-                    >
-                      {" "}
-                      UX Hosts
-                    </NavLink>
-                    <NavLink
-                      className={"nav-link"}
-                      activeClassName={"active"}
-                      to={"/events"}
-                      style={{
-                        WebkitTransform: `translate3d(${-w}px, 0, 0)`,
-                        transform: `translate3d(${-w}px, 0, 0)`,
-                        opacity: o
-                      }}
-                    >
-                      {" "}
-                      UX Events
-                    </NavLink>
-                    <NavLink
-                      className={"nav-link"}
-                      activeClassName={"active"}
-                      to={"/get-in-touch"}
-                      style={{
-                        WebkitTransform: `translate3d(${-z}px, 0, 0)`,
-                        transform: `translate3d(${-z}px, 0, 0)`,
-                        opacity: o
-                      }}
-                    >
-                      {" "}
-                      Get In Touch
+                  />
+                </div>
+            )}
+          </Motion>
+
+          <Motion style={{ x: spring(drawerOpened ? 1 : 0) }}>
+            {({ x }) => (
+                <div
+                    className="drawer"
+                    style={{
+                      opacity: x,
+                      pointerEvents: drawerOpened ? "all" : "none"
+                    }}
+                >
+                  <div className="brand">
+                    <NavLink to={"/"}>
+                      <LogoWhite />
                     </NavLink>
                   </div>
-                )}
-              </Motion>
-            </div>
-          )}
-        </Motion>
+                  <Motion
+                      style={{
+                        x: spring(drawerOpened ? 0 : 200),
+                        y: spring(drawerOpened ? 0 : 400),
+                        w: spring(drawerOpened ? 0 : 600),
+                        z: spring(drawerOpened ? 0 : 800),
+                        o: spring(drawerOpened ? 1 : 0)
+                      }}
+                  >
+                    {({ x, y, w, z, o }) => (
+                        <div className="menu-items">
+                          <NavLink
+                              exact
+                              className={"nav-link"}
+                              activeClassName={"active"}
+                              to={"/"}
+                              style={{
+                                WebkitTransform: `translate3d(${-x}px, 0, 0)`,
+                                transform: `translate3d(${-x}px, 0, 0)`,
+                                opacity: o
+                              }}
+                          >
+                            {" "}
+                            Hi!
+                          </NavLink>
+                          <NavLink
+                              className={"nav-link"}
+                              activeClassName={"active"}
+                              to={"/talents"}
+                              style={{
+                                WebkitTransform: `translate3d(${-y}px, 0, 0)`,
+                                transform: `translate3d(${-y}px, 0, 0)`,
+                                opacity: o
+                              }}
+                          >
+                            {" "}
+                            UX Talents
+                          </NavLink>
+                          <NavLink
+                              className={"nav-link"}
+                              activeClassName={"active"}
+                              to={"/hosts"}
+                              style={{
+                                WebkitTransform: `translate3d(${-y}px, 0, 0)`,
+                                transform: `translate3d(${-y}px, 0, 0)`,
+                                opacity: o
+                              }}
+                          >
+                            {" "}
+                            UX Hosts
+                          </NavLink>
+                          <NavLink
+                              className={"nav-link"}
+                              activeClassName={"active"}
+                              to={"/events"}
+                              style={{
+                                WebkitTransform: `translate3d(${-w}px, 0, 0)`,
+                                transform: `translate3d(${-w}px, 0, 0)`,
+                                opacity: o
+                              }}
+                          >
+                            {" "}
+                            UX Events
+                          </NavLink>
+                          <NavLink
+                              className={"nav-link"}
+                              activeClassName={"active"}
+                              to={"/get-in-touch"}
+                              style={{
+                                WebkitTransform: `translate3d(${-z}px, 0, 0)`,
+                                transform: `translate3d(${-z}px, 0, 0)`,
+                                opacity: o
+                              }}
+                          >
+                            {" "}
+                            Get In Touch
+                          </NavLink>
+                        </div>
+                    )}
+                  </Motion>
+                </div>
+            )}
+          </Motion>
+        </div>
       </div>
     </Affix>
   );
